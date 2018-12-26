d90d809bbf78c9bb62b9b7104c1364bb83af959d
Zyro9922 <alihasan9922@gmail.com>
2018-12-26 19:19:31 +0530

    3 changes done

---
+++ app/src/main/java/com/example/alihasan/synergytwo/api/service/ServerURL.java
@@ -0,0 +1,10 @@
+package com.example.alihasan.synergytwo.api.service;
+
+public class ServerURL {
+
+    private String SERVER_URL = "http://159.65.155.5/android/";
+
+    public String getSERVER_URL() {
+        return SERVER_URL;
+    }
+}
