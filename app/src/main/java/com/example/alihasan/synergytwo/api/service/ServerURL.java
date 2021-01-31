13bdbbc9a43f849d2a017a34c2190d36e2d90f93
Zyro9922 <alihasan9922@gmail.com>
2021-01-31 17:38:37 +0530

    Fixing

--- app/src/main/java/com/example/alihasan/synergytwo/api/service/ServerURL.java
+++ app/src/main/java/com/example/alihasan/synergytwo/api/service/ServerURL.java
@@ -2,7 +2,7 @@ package com.example.alihasan.synergytwo.api.service;
 
 public class ServerURL {
 
-    private String SERVER_URL = "http://142.93.222.115/android/";
+    private String SERVER_URL = "http://520f0dae506c.ngrok.io/aztek/android/";
 
     public String getSERVER_URL() {
         return SERVER_URL;
